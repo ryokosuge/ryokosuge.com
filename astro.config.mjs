bf73834ce383042820dbb5478d2962eec52405f2
ryokosuge <ryo.kosuge@gmail.com>
2022-12-08 21:44:28 +0900

    chore:	yarn astro add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://blog.ryokosuge.com",
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()]
 });
