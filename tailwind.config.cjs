a7c72534b3b6cb7d322d6f7164d142fe4d26f5ab
ryokosuge <ryo.kosuge@gmail.com>
2023-01-06 18:37:56 +0900

    feat:	base layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: colors.gray["100"],
+          medium: colors.gray["200"],
+          dark: colors.gray["300"],
+        },
+      },
+      fontFamily: {
+        sans: ["Ubuntu", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
