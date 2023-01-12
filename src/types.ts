8f2da50ad14dcbf71ba14416d5278834e9d1543a
ryokosuge <ryo.kosuge@gmail.com>
2023-01-12 14:21:40 +0900

    feat:	layout & API Schema

--- src/types.ts
+++ src/types.ts
@@ -5,12 +5,28 @@ export type Article = {
   publishedAt: string;
   revisedAt: string;
   title: string;
-  content: string;
-  eyecatch?: ArticleEyecatch;
+  description: string;
+  sections: ArticleSection[];
 };
 
-export type ArticleEyecatch = {
-  url: string;
-  width: number;
-  height: number;
+export const FieldID = {
+  ARTICLE_SECTION_TEXT: "article_section_text",
+  ARTICLE_SECTION_CODE: "article_section_code",
+} as const;
+
+export type FieldIDType = typeof FieldID[keyof typeof FieldID];
+
+export type ArticleSection = ArticleSectionText | ArticleSectionCode;
+
+export type ArticleSectionText = {
+  fieldId: "article_section_text";
+  body: string;
 };
+
+export type ArticleSectionCode = {
+  fieldId: "article_section_code";
+  lang: ArticleSectionCodeLang[];
+  code: string;
+};
+
+export type ArticleSectionCodeLang = "go" | "typescript";
