16e01e950a2221ac52bbcfd20eb6fc122f983c12
ryokosuge <ryo.kosuge@gmail.com>
2023-01-13 11:27:53 +0900

    feat:	edit config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,21 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'log.Printf("%s", title)';
+export const SITE_TITLE = "Blog.";
 export const SITE_DESCRIPTION = "どうも、こんにちは！";
+
+type SocialLink = {
+  name: string;
+  link: string;
+};
+
+export const SITE_HEADER_SOCIAL_LINKS: SocialLink[] = [
+  {
+    name: "twitter",
+    link: "https://twitter.com/_ryokosuge",
+  },
+  {
+    name: "github",
+    link: "https://github.com/ryokosuge",
+  },
+];
