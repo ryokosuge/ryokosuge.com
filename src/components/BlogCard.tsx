7a2d107e712930eda2ee9548b7ec4a65279f9dda
ryokosuge <ryo.kosuge@gmail.com>
2023-03-23 12:55:09 +0900

    feat:	コンテンツとスタイルの修正

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -19,12 +19,14 @@ export const BlogCard: React.FC<Props> = ({
         rel="noopener nofollow"
         className="not-prose p-2 sm:p-4 flex flex-col md:flex-row gap-4 md:gap-6 no-underline hover:underline items-center border rounded-lg shadow-md hover:bg-gray-100"
       >
-        {imageUrl && (
+        {imageUrl ? (
           <img
             className="object-cover rounded-lg h-48 sm:h-64 md:h-auto w-auto md:w-48"
             src={imageUrl}
             alt=""
           />
+        ) : (
+          <div className="object-cover rounded-lg h-48 sm:h-64 md:h-auto w-full md:w-48 md:aspect-video bg-slate-300" />
         )}
         <div className="flex flex-col gap-1">
           <p className="text-base sm:text-lg font-bold text-primary-medium">
