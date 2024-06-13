3a5344cf07d3e12a9887ae1276872efdd7fe4106
ryokosuge <ryo.kosuge@gmail.com>
2024-06-13 10:46:17 +0900

    Update npm dependencies and remove unused code
    - Remove unused dependency "@uidotdev/usehooks" from package.json and pnpm-lock.yaml
    - Update "cheerio" dependency to version 1.0.0-rc.12
    - Update "html-react-parser" dependency to version 5.1.10
    - Remove "NavMenu" component and related code from the project
    - Update "layout.tsx" to remove the usage of "NavMenu" component
    - Update "page.tsx" to add a new link to the blog page
    - Update "NavMenu.tsx" to remove the file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
 import { Hero } from "@/components/Hero";
+import Link from "next/link";
 
 const Page = () => {
-  return <Hero title="Home" description="メモ書き用で雑に書いてます。" />;
+  return (
+    <>
+      <Hero title="Home" description="メモ書き用で雑に書いてます。" />
+      <ul>
+        <li>
+          <Link
+            href="/blog"
+            className="text-primary-dark font-semibold text-lg hover:underline md:sticky md:top-0 md:pt-4"
+          >
+            Blog. →
+          </Link>
+        </li>
+      </ul>
+    </>
+  )
 };
 
 export default Page;
