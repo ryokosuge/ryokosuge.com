735672c091932c5e84f042e18262b32379e9e496
ryokosuge <ryokosuge@users.noreply.github.com>
2024-06-17 22:45:10 +0900

    chore: Update font loading in layout.tsx and tailwind.config.ts (#295)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,25 @@ import "../styles/globals.css";
 
 import type { Metadata } from "next";
 import Link from "next/link";
+import { Noto_Sans_JP, Open_Sans } from "next/font/google";
 import { SOCIAL_LINKS } from "../constants/social";
 
 export const metadata: Metadata = {
   title: "Home | ryokosuge.com",
   description: "メモ書き用で雑に書いてます。",
 };
+
+const noto_sans_jp = Noto_Sans_JP({
+  display: "optional",
+  variable: "--font-noto-sans-jp",
+  preload: false,
+});
+
+const open_sans = Open_Sans({
+  display: "optional",
+  variable: "--font-open-sans",
+  preload: false,
+});
 
 export default function RootLayout({
   children,
@@ -17,7 +30,7 @@ export default function RootLayout({
   const today = new Date();
 
   return (
-    <html lang="ja">
+    <html lang="ja" className={`${noto_sans_jp.variable} ${open_sans.variable} font-noto-sans-jp`}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
@@ -43,12 +56,8 @@ export default function RootLayout({
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
-        <link
-          href="https://fonts.googleapis.com/css?family=Noto+Sans+JP|Open+Sans&display=optional"
-          rel="stylesheet"
-        />
       </head>
-      <body className="min-h-[100vh] m-auto bg-slate-50 box-border border-solid border-primary-dark border-[0.75rem] sm:border-[1rem]">
+      <body className="font-noto-sans-jp min-h-[100vh] m-auto bg-slate-50 box-border border-solid border-primary-dark border-[0.75rem] sm:border-[1rem]">
         <div className="max-w-7xl m-auto p-8 md:p-14">
           <header className="mb-8 sm:mb-12 md:mb-16 flex justify-between items-center text-lg md:text-xl text-primary-medium">
             <Link
